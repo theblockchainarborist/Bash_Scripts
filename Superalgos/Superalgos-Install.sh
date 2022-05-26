0f128a321ed45d9264b7f7c2ee8036614f29059f
theblockchainarborist <89878622+theblockchainarborist@users.noreply.github.com>
2022-05-25 22:05:52 -0400

    Superalgos Install Script Start

---
+++ Superalgos/Superalgos-Install.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# First Lets Clear the Terminal.
+clear
+
+# Welcome Message
+echo "----------------------------------------"
+echo "|  Welcome to the Superalgos Install!  |"
+echo "----------------------------------------"
+echo " "
+echo " "
+
+echo "This script is intened to be used to install Superalgos on Ubuntu"
+echo "After a few questions this script will:"
+echo " "
+echo " .........Check and install / update all dependencies"
+echo " .........Clone your Superalgos Fork to this machine"
+echo " .........Setup Superalgos Plugins"
+
+echo " "
+echo " "
+echo " "
+
+# Three Strikes and your Out.
+counter=1
+#Ask for the needed user input.
+for (x = 3;)
+
+
